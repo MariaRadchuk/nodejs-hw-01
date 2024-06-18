569f311a0242b85c9f2a646d43318bf9ea74098a
MariaRadchuk <mradchuk_15@ukr.net>
2024-06-18 22:40:29 +0300

    2

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,7 +1,5 @@
 import path from 'node:path';
 
-//Get the current work catalogue
 const pathToWorkDir = path.join(process.cwd());
 
-//A variable is declared that stores the path to the file src/db/db.jso
 export const PATH_DB = path.join(pathToWorkDir, 'src', 'db', 'db.json');
